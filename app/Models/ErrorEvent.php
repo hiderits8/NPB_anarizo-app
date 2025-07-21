3ff1dfa7fb08107b56580f8ed6135a7fbdb06608
hiderits8 <128969706+hiderits8@users.noreply.github.com>
2025-07-21 22:08:12 +0900

    fix(models): add use statements

--- app/Models/ErrorEvent.php
+++ app/Models/ErrorEvent.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\Player;
+use App\Models\PlayByPlay;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
